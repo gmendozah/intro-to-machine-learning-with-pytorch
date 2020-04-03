11081f72c2986e2d1176a6a2937d92dfa2cfd65e
gmendozah <geovanimendoza.h@gmail.com>
2020-04-03 12:37:28 -0400

    solve regularization exercise

---
+++ supervised-learning/linear-regretion/regularization.py
@@ -0,0 +1,20 @@
+# TODO: Add import statements
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import Lasso
+
+# Assign the data to predictor and outcome variables
+# TODO: Load the data
+train_data = pd.read_csv('reg_dataset.csv', header = None)
+X = train_data.iloc[:,:-1]
+y = train_data.iloc[:,-1]
+
+# TODO: Create the linear regression model with lasso regularization.
+lasso_reg = Lasso()
+
+# TODO: Fit the model.
+lasso_reg.fit(X, y)
+
+# TODO: Retrieve and print out the coefficients from the regression model.
+reg_coef = lasso_reg.coef_
+print(reg_coef)
