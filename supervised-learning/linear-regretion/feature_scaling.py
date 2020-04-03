f10514056f2b2a0e1f90571544f3d426e00185ab
gmendozah <geovanimendoza.h@gmail.com>
2020-04-03 12:41:44 -0400

    solve feature scaling exercise

---
+++ supervised-learning/linear-regretion/feature_scaling.py
@@ -0,0 +1,27 @@
+# TODO: Add import statements
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import Lasso
+from sklearn.preprocessing import StandardScaler
+
+# Assign the data to predictor and outcome variables
+# TODO: Load the data
+train_data = pd.read_csv('fa_dataset.csv', header = None)
+X = train_data.iloc[:,:-1]
+y = train_data.iloc[:,-1]
+
+# TODO: Create the standardization scaling object.
+scaler = StandardScaler()
+
+# TODO: Fit the standardization parameters and scale the data.
+X_scaled = scaler.fit_transform(X)
+
+# TODO: Create the linear regression model with lasso regularization.
+lasso_reg = Lasso()
+
+# TODO: Fit the model.
+lasso_reg.fit(X_scaled, y)
+
+# TODO: Retrieve and print out the coefficients from the regression model.
+reg_coef = lasso_reg.coef_
+print(reg_coef)
